e91cc40a20015e012ef6f1e04711a330de8f8db9
hordecore <hordecore@oleg.(none)>
2012-12-09 20:07:33 +0600

    Working with 0

--- main.c
+++ main.c
@@ -20,13 +20,30 @@ void init_path(struct path arr[10][10])
 	int i,j;
 	for (i=1;i<10;i++)
 		for (j=1;j<10;j++)
-			if (i==j) {
-				add_path(&array[j][i], 0, 0);
-				printf("add %d %d: 0 0\n", i, j);
-			} else {
-				add_path(&array[j][i], 0, -1);
-				printf("add %d %d: 0 -1\n", i, j);
-			}
+			if (i==j) 
+				add_path(&arr[j][i], 0, 0);
+			else
+				add_path(&arr[j][i], 0, -1);
+	add_path(&arr[1][2], 0, 8);
+	add_path(&arr[2][3], 0, 4);
+	add_path(&arr[3][5], 0, 8);
+	add_path(&arr[3][1], 0, 2);
+	add_path(&arr[4][3], 0, 5);
+	add_path(&arr[4][6], 0, 5);
+	add_path(&arr[5][4], 0, 4);
+	add_path(&arr[5][6], 0, 6);
+	add_path(&arr[6][1], 0, 7);
+	add_path(&arr[6][3], 0, 3);
+	add_path(&arr[6][8], 0, 1);
+	add_path(&arr[7][5], 0, 1);
+	add_path(&arr[7][6], 0, 2);
+	add_path(&arr[7][8], 0, 7);
+	add_path(&arr[8][7], 0, 1);
+	add_path(&arr[8][1], 0, 5);
+	add_path(&arr[8][9], 0, 2);
+	add_path(&arr[9][1], 0, 1);
+	add_path(&arr[9][8], 0, 3);
+
 	return;
 }
 
@@ -42,6 +59,14 @@ void print_path_array(struct path arr[10][10])
 		printf("\n");
 	}
 
+	return;
+}
+
+void print_avaible(struct path arr[10][10], int from) {
+	int i,j;
+	for (i=1;i<10;i++)
+		if (arr[from][i].cost > 0 && arr[from][i].by == 0)
+			printf("%d->%d: %d\n", from, i, arr[from][i].cost);
 	return;
 }
 
