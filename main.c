6aa245365af64c30f493007348059890b4aec20c
hordecore <hordecore@oleg.(none)>
2012-12-09 20:46:31 +0600

    Неправильный by, надо не с конца вычислять, а с 0 и ++, а не --

--- main.c
+++ main.c
@@ -24,6 +24,7 @@ void init_path(struct path arr[10][10])
 				add_path(&arr[j][i], 0, 0);
 			else
 				add_path(&arr[j][i], 0, -1);
+	/* дано из задачи */
 	add_path(&arr[1][2], 0, 8);
 	add_path(&arr[2][3], 0, 4);
 	add_path(&arr[3][5], 0, 8);
@@ -62,7 +63,8 @@ void print_path_array(struct path arr[10][10])
 	return;
 }
 
-void print_avaible(struct path arr[10][10], int from) {
+void print_avaible(struct path arr[10][10], int from) 
+{
 	int i,j;
 	for (i=1;i<10;i++)
 		if (arr[from][i].cost > 0 && arr[from][i].by == 0)
@@ -70,10 +72,35 @@ void print_avaible(struct path arr[10][10], int from) {
 	return;
 }
 
+int eval_path_by(int from, int to, struct path p1, struct path p2) {
+	if (from == to)
+		return 0;
+	printf("\nfrom: %d to %d:\n", from, to);
+	printf("p1.by: %d\np2.by: %d\n\n", p1.by, p2.by);
+	return 0;
+}
+
+void print_avaible_by(struct path arr[10][10], int orig, int from, int by) 
+{
+	int i,j;
+	if (by < 0)
+		return;
+	for (i=1;i<10;i++) {
+		if (arr[from][i].cost > 0 && orig != i) {
+			printf("%d->%d: %d", orig, i, arr[orig][from].cost + arr[from][i].cost);
+			printf("  path: %d -> %d -> %d\n", orig, from, i);
+			add_path(&arr[orig][i], arr[orig][from].cost + arr[from][i].cost, by);
+			print_avaible_by(arr, orig, i, by-1);
+		}
+	}
+
+}
+
 void main(void)
 {
 	init_path(array);
 	init_path(tmp);
 	print_path_array(array);
+	print_avaible_by(array, 2, 2, 1);
 	return;
 }
